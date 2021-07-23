34119aecc9842fa3be864d45fc57b5cd044bf5fa
Joan Rasmussen <raz.joan@gmail.com>
2021-07-23 07:59:55 -0600

    Add vehicle object to modelB

---
+++ models/modelB.js
@@ -0,0 +1,19 @@
+// mod0 assessment: Part 3 FEE: joan ramussen
+
+var forester = {
+  make: "Subaru",
+  model: "Forester",
+  year: 2014,
+  miles: 81000,
+  isAutomatic: true,
+  hasSunroof: false,
+  powerItems: ["windows", "locks"],
+  maintenaceLogDates: ["03/13/15", "10/01/15", "4/10/16", "10/20/16", "03/01/17", "11/14/17", "05/30/18", "01/12/19", "09/02/19", "03/19/20", "11/01/20", "05/24/21"],
+  owner: {
+    name: "Joan Rasmussen",
+    phoneNumber: "(337)123-4567",
+    email: "raz.joan@gmail.com"
+  }
+}
+
+console.log(forester);
