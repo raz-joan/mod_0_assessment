a3664d6f3d8c44a4976a3d30f7f60262094e576d
Joan Rasmussen <raz.joan@gmail.com>
2021-07-23 07:48:29 -0600

    Add airport object to modelA

---
+++ models/modelA.js
@@ -0,0 +1,14 @@
+// mod0 assessment: Part 3 FEE: joan rasmussen
+
+var denverInternationalAirport = {
+  name: "Denver International Airport",
+  website: "www.flydenver.com",
+  numberOfTerminals: 2,
+  numberOfGates: 3,
+  isOpen: true,
+  hasSecurityScreening: true,
+  terminalNames: ["Terminal West", "Terminal East"],
+  gateNames: ["A", "B", "C"]
+}
+
+console.log(denverInternationalAirport);
