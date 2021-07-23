3cf2e4578592567458ae9552c9bf4266d0dcb076
Joan Rasmussen <raz.joan@gmail.com>
2021-07-23 07:36:03 -0600

    Complete program specific challenge

---
+++ variable.js
@@ -0,0 +1,25 @@
+// Mod 0 Assessment: Part 2 (FEE): Joan Rasmussen
+
+var famousPerson = "Tom Hanks";
+
+var isActive = true;
+
+famousPerson = "Beyonce";
+
+var num3 = 3;
+
+var num9 = 9;
+
+var sum = num3 + num9;
+
+// console.log(sum);
+
+var animals = ["alligator", "flamingo", "bear"];
+
+// console.log(animals);
+
+animals.shift();
+
+// console.log(animals);
+
+console.log(famousPerson + " has " + famousPerson.length + " characters in their name.");
